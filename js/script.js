082c5ece6c8fd9b74a6d8149ce3cb57d47738a4b
i-webtest <stasbass80@gmail.com>
2023-02-01 11:22:11 +0300

    Completed day2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 import { initControl } from './control.js';
+import { state } from './state.js';
 
 const initPomodoro = () => {
   initControl();
+
+  state.activeTodo = {
+    id: 'default',
+    pomodoro: 3,
+    title: 'Помодоро',
+  };
 };
 
 initPomodoro();
